34cf7a12e9403f72a61e2dab54608180aaf3d1a7
Benjamin Lei <benlei.cse@gmail.com>
2020-09-12 02:29:12 -0700

    fff will just move it all to root + rename my dir

--- templatetags/mixins.py
+++ templatetags/mixins.py
@@ -2,8 +2,8 @@ from django import template
 from django.template import TemplateSyntaxError
 from django.template.base import token_kwargs
 
-from mixin_templatetag.mixinnode import MixinNode
-from mixin_templatetag.mixnode import MixNode
+from mixinnode import MixinNode
+from mixnode import MixNode
 
 register = template.Library()
 
