70a97f687b5f484da349c87072aec92ce8a7ddd2
Benjamin Lei <benlei.cse@gmail.com>
2020-09-12 14:54:43 -0700

    to avoid some potentially odd behavior, require mixins at the top

--- mixin_templatetag/__init__.py
+++ mixin_templatetag/__init__.py
@@ -3,4 +3,4 @@
 
 __author__ = 'Benjamin Lei'
 
-__version__ = '0.2.0'
+__version__ = '0.2.1'
--- mixin_templatetag/mixinnode.py
+++ mixin_templatetag/mixinnode.py
@@ -2,5 +2,7 @@ from django import template
 
 
 class MixinNode(template.Node):
+    must_be_first = True
+
     def render(self, context):
         return ''
